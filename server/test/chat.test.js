03f82c750dabcc262f382bf025c99860908d671c
Fadhil Muhammad <fadhilmuhammad@Fadhils-M1-MacBook-Pro.local>
2021-07-27 18:02:00 +0700

    add test

--- server/test/chat.test.js
+++ server/test/chat.test.js
@@ -1,20 +1,321 @@
-const ChatController = require('../controllers/chatController')
 const axios = require('axios')
 const app = require('../app')
-const { request } = require('../app')
+const request = require('supertest')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 jest.mock('axios')
 
 describe('Should able to return reply', () => {
-  test('Should send a reply in string', (done) => {
-    const reply = "Ask me something like 'what does navbar mear?' or 'what is footer?'"
-    const resp = { message: reply}
-    axios.get.mockResolvedValue(resp)
-    const req = {
-      body: 'Hello'
+  test('Should send a reply for main explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "main section?",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9988
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 12,
+                    "body": "main section",
+                    "confidence": 0.993,
+                    "entities": [],
+                    "value": "main section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
     }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is main section?'
     
-    return ChatController.postChat(_, (req, res))
-      .then()
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('Main section is the first thing visitors will see when they visit your website. Putting catchy and interesting image or text in this section will make your visitors stay longer in your website!')
+          done()
+        }
+      })
+  })
+
+  test('Should send a reply for main section explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "What is navbar??",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9876
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 6,
+                    "body": "navbar",
+                    "confidence": 0.9835,
+                    "entities": [],
+                    "value": "navbar section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is navbar?'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('A navigation bar is a user interface element within a webpage that contains links to other sections of the website. It is most commonly displayed as horizontal list of links at the top of each page.')
+          done()
+        }
+      })
+  })
+
+  test('Should send a reply for about section explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "What is about section?",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9876
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 6,
+                    "body": "navbar",
+                    "confidence": 0.9835,
+                    "entities": [],
+                    "value": "about section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is about section?'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('This is where you put the introduction of your company. Introduce your company to visitors by putting a simple description text about your company.')
+          done()
+        }
+      })
+  })
+
+  test('Should send a reply for service section explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "What is service section?",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9876
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 6,
+                    "body": "navbar",
+                    "confidence": 0.9835,
+                    "entities": [],
+                    "value": "service section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is service section?'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('Service section is where you can introduce your products to visitors. Got any products to show? Let visitors to know about your products here!')
+          done()
+        }
+      })
+  })
+
+  test('Should send a reply for contact section explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "What is contact section?",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9876
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 6,
+                    "body": "navbar",
+                    "confidence": 0.9835,
+                    "entities": [],
+                    "value": "contact section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is contact section?'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('Let your visitors know how to reach you. You can put your company email, phone, or address here.')
+          done()
+        }
+      })
+  })
+
+  test('Should send a reply for footer section explanation in string', (done) => {
+    const resp = {
+      data: {
+        "text": "What is footer section?",
+        "intents": [
+            {
+                "id": "3708455829379077",
+                "name": "sectionMeaning",
+                "confidence": 0.9876
+            }
+        ],
+        "entities": {
+            "section:section": [
+                {
+                    "id": "1052047131998415",
+                    "name": "section",
+                    "role": "section",
+                    "start": 0,
+                    "end": 6,
+                    "body": "navbar",
+                    "confidence": 0.9835,
+                    "entities": [],
+                    "value": "footer section",
+                    "type": "value"
+                }
+            ]
+        },
+        "traits": {}
+      }
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'What is footer section?'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual('Footer is the bottom part of your website. Commonly, social media links are put here.')
+          done()
+        }
+      })
+  })
+
+  test('Should send a default reply', (done) => {
+    const resp = {
+      data: {}
+    }
+    axios.mockResolvedValue(resp)
+    const chat = 'asdasd asd kajnsfda'
+    
+    request(app)
+      .post('/chat')
+      .send({ chat })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('message', expect.any(String))
+          expect(res.body.message).toEqual("Ask me something like 'what does navbar mear?' or 'what is footer?'")
+          done()
+        }
+      })
   })
 })
--- server/test/user.test.js
+++ server/test/user.test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 const app = require('../app')
 const { User } = require('../models')
 
+jest.mock('axios')
 
 let user_data = {
   email: 'test@mail.com',
@@ -259,56 +260,35 @@ describe('User Google Login [ERROR CASE]', () => {
   })
 })
 
-// jest.mock('axios')
-// describe('User Google Login [SUCCESS CASE]', () => {
+describe('User Google Login [SUCCESS CASE]', () => {
   
-//   test('should fetch users', () => {
-//     class Users {
-//       static all() {
-//         return new Promise((resolve, reject) => {
-//           return (
-//             axios({
-//               url: '/googleLogin',
-//               method: 'POST',
-//               data: {
-//                 idToken: '1035521074618-nkotpceb3p60muu0h5rmf6hn5pe72dtc.apps.googleusercontent.com'
-//               }
-//             })
-//               .then(resp => {
-//                 resp.data
-//                 resolve(resp.data)
-//               })
-//           )
-//         })
-//       }
-//     }
+  test('Should send an object with key: access_token, id, email', (done) => {
+    const resp = {
+      data: {
+        access_token: 'asdasdasdasd',
+        id: '1',
+        email: 'user@mail.com'
+      }
+    }
+    axios.mockResolvedValue(resp)
     
-//     const users = [{name: 'Bob'}];
-//     const resp = {data: users};
-//     axios.get.mockResolvedValue(resp);
-  
-//     return Users.all().then(data => expect(data).toEqual(users));
-//   });
-  
-  // test('Should send an object with key: access_token', (done) => {
-  //   class Users {
-  //     static all() {
-  //       return axios.post('/googleLogin')
-  //         .then(resp => resp.data);
-  //     }
-  //   }
-  //   const users = [{name: 'Bob'}];
-  //   // const resp = 'string'
-  //   const resp = {data: users};
-  //   axios.get.mockResolvedValue(resp)
+    request(app)
+      .post('/googleLogin')
+      .send({
+        token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjNkZjBhODMxZTA5M2ZhZTFlMjRkNzdkNDc4MzQ0MDVmOTVkMTdiNTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiMTAzNTUyMTA3NDYxOC1ua290cGNlYjNwNjBtdXUwaDVybWY2aG41cGU3MmR0Yy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjEwMzU1MjEwNzQ2MTgtbmtvdHBjZWIzcDYwbXV1MGg1cm1mNmhuNXBlNzJkdGMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDQ0ODQwMDg0MzgxMDAyMzkzMDEiLCJlbWFpbCI6ImZhZGhpbG1obW1kQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiY194MWR4RHY0QW1Qc2lMSHlTUGRIQSIsIm5hbWUiOiJGYWRoaWwgTXVoYW1tYWQiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2pUbk0yMTM4dlZxTzZmc2dYQlBWcG5kTkZmUVhsM0EyamVwdUphY1E9czk2LWMiLCJnaXZlbl9uYW1lIjoiRmFkaGlsIiwiZmFtaWx5X25hbWUiOiJNdWhhbW1hZCIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjI3MzczODE1LCJleHAiOjE2MjczNzc0MTUsImp0aSI6ImNjY2M1OTIxOGMxZTlmMTUxMTE2M2M1NTlhZTI4ZWZhMmFhZDk3MWUifQ.MztE3jDzY7Mz-13PTSOOperXU2LBlF34_QxSp1REgW8LJiBr3SPP4Pg_QjICoEJLkzC3T7a-aFgkY5rxDuLPT8LDDG8Emcrm7WhuPcci737SQIEwYTHk1rQaWPicBlLneBM-4Ic7qeuXOYh5JNxEZmg3QpkPwyg4oTwtxzFYcWpxXE4m02ESR_Uo8SLxmFidMunUoAyIl6EdsLTXY74Gy5hnBJH9CA2Qfvk09a5dUdsmEApevB00MnkrnufeFu8CE1aJkByinyuRgNT1mqy6d8UG6rsYTWRdumC1rPqXIKwTPpEsQyORMDBdmLsx3au2QlX4JAiUXs9AnV8jcEytRA'
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        } else {
+          console.log(res.body);
+          expect(res.status).toBe(200)
+          expect(res.body).toHaveProperty('access_token', expect.any(String))
+          expect(res.body).toHaveProperty('id', expect.any(Number))
+          expect(res.body).toHaveProperty('email', expect.any(String))
+          done()
+        }
+      })
+  })
 
-  //   return Users.all().then(data => {
-  //     expect(data).toEqual(users)
-  //     done()
-  //   })
-  // })
-// })
-
-// describe('User Google Login [ERROR CASE]', () => {
-//   test.skip('Failed because')
-// })
+})
