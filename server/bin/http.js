7f5b44e031bf33d8c4f21c7f84d2757a7563c165
sanjungliu <sanjungliu@gmail.com>
2021-07-28 14:43:46 +0700

    deploy configuration

--- server/bin/http.js
+++ server/bin/http.js
@@ -1,3 +1,5 @@
-const app = require('../app')
-const port = 4000
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+const app = require("../app");
+const port = process.env.PORT || 4000;
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -8,49 +8,47 @@ class ChatController {
       url: `https://api.wit.ai/message?q=${chat}`,
       method: "GET",
       headers: {
-        Authorization: "Bearer PA72X3OCGU7CBYVPC4ZCMS4PBSQLRQYB",
+        Authorization: process.env.WIT_AI_TOKEN,
       },
-    })
-      .then(({ data }) => {
-        if (data.entities["section:section"]) {
-          if (data.entities["section:section"][0].value === "navbar section") {
-            reply =
-              "A navigation bar is a user interface element within a webpage that contains links to other sections of the website. It is most commonly displayed as horizontal list of links at the top of each page.";
-          } else if (
-            data.entities["section:section"][0].value === "main section"
-          ) {
-            reply =
-              "Main section is the first thing visitors will see when they visit your website. Putting catchy and interesting image or text in this section will make your visitors stay longer in your website!";
-          } else if (
-            data.entities["section:section"][0].value === "about section"
-          ) {
-            reply =
-              "This is where you put the introduction of your company. Introduce your company to visitors by putting a simple description text about your company.";
-          } else if (
-            data.entities["section:section"][0].value === "service section"
-          ) {
-            reply =
-              "Service section is where you can introduce your products to visitors. Got any products to show? Let visitors to know about your products here!";
-          } else if (
-            data.entities["section:section"][0].value === "contact section"
-          ) {
-            reply =
-              "Let your visitors know how to reach you. You can put your company email, phone, or address here.";
-          } else if (
-            data.entities["section:section"][0].value === "footer section"
-          ) {
-            reply =
-              "Footer is the bottom part of your website. Commonly, social media links are put here.";
-          }
-        } else {
+    }).then(({ data }) => {
+      if (data.entities["section:section"]) {
+        if (data.entities["section:section"][0].value === "navbar section") {
           reply =
-              "Ask me something like 'what does navbar mean?' or 'what is footer?'";
+            "A navigation bar is a user interface element within a webpage that contains links to other sections of the website. It is most commonly displayed as horizontal list of links at the top of each page.";
+        } else if (
+          data.entities["section:section"][0].value === "main section"
+        ) {
+          reply =
+            "Main section is the first thing visitors will see when they visit your website. Putting catchy and interesting image or text in this section will make your visitors stay longer in your website!";
+        } else if (
+          data.entities["section:section"][0].value === "about section"
+        ) {
+          reply =
+            "This is where you put the introduction of your company. Introduce your company to visitors by putting a simple description text about your company.";
+        } else if (
+          data.entities["section:section"][0].value === "service section"
+        ) {
+          reply =
+            "Service section is where you can introduce your products to visitors. Got any products to show? Let visitors to know about your products here!";
+        } else if (
+          data.entities["section:section"][0].value === "contact section"
+        ) {
+          reply =
+            "Let your visitors know how to reach you. You can put your company email, phone, or address here.";
+        } else if (
+          data.entities["section:section"][0].value === "footer section"
+        ) {
+          reply =
+            "Footer is the bottom part of your website. Commonly, social media links are put here.";
         }
-        res.status(200).json({ message: reply });
-        return reply;
-      })
+      } else {
+        reply =
+          "Ask me something like 'what does navbar mean?' or 'what is footer?'";
+      }
+      res.status(200).json({ message: reply });
+      return reply;
+    });
   }
-
 }
 
 module.exports = ChatController;
